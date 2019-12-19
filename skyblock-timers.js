ed8b72397cf450a995ba9411049fecc984b88945
inventivetalent <mail@inventivetalent.org>
2019-12-19 12:08:58 +0100

    Finally being lazy enough to just create a central generator for all timers instead of copy-pasting

---
+++ skyblock-timers.js
@@ -0,0 +1,50 @@
+module.exports = {
+    newyear: {
+        name: "New Year's",
+        path: "skyblock-newyear-timer",
+        image: "Cake.png",
+        ad: "8836152197",
+        endpoint: "skyblock/newyear/estimate",
+        refreshInterval: 60000,
+        imageCount: 3
+    },
+    spookyfestival: {
+        name: "Spooky Festival",
+        path: "skyblock-spooky-festival-timer",
+        image: "jack_o_lantern.png",
+        ad: "6605372387",
+        endpoint: "skyblock/spookyFestival/estimate",
+        refreshInterval: 60000,
+        imageCount: 3
+    },
+    darkauction: {
+        name: "Dark Auction",
+        path: "skyblock-dark-auction-timer",
+        image: "auction.png",
+        ad: "4893037759",
+        endpoint: "skyblock/darkauction/estimate",
+        refreshInterval: 60000,
+        imageCount: 5
+    },
+    winter: {
+        name: "Winter",
+        path: "skyblock-winter-timer",
+        image: "Snowball.png",
+        ad: "1028315874",
+        endpoint: "skyblock/winter/estimate",
+        refreshInterval: 60000,
+        imageCount: 3
+    },
+    interest: {
+        name: "Bank Interest",
+        path: "skyblock-bank-interest-timer",
+        image: "gold_bag.png",
+        ad: "4388165947",
+        endpoint: "skyblock/bank/interest/estimate",
+        refreshInterval: 60000,
+        timerTextPre: "You will receive interest in about",
+        timerTextActive: "You should receive interest",
+        timerTextActiveValue: "NOW",
+        imageCount: 3
+    },
+};
